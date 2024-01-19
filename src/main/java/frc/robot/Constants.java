bac852b3da05ca8b3f4ffd55b32cb6e75bb53489
Samuel Bowling <ssb00103@ksad-fk42m61wfv.ks.k12itc.us>
2024-01-18 18:47:02 -0600

    Switched to new repository

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+
+public class Constants {
+    public class OperatorConstants {
+        public static final int LEFT_JOYSTICK_PORT = 0;
+        public static final int RIGHT_JOYSTICK_PORT = 1;
+
+    
+        
+    }
+    public class DriveConstants {
+        public static final int LEFT_MOTORS = 0;
+        public static final boolean LEFT_INVERTED = true;
+
+        public static final int RIGHT_MOTORS = 1;
+        public static final boolean RIGHT_INVERTED = false;
+
+        public static final double DEADBAND = .05;
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,47 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveSubsystem extends SubsystemBase {
+    
+    private Talon leftMotors;
+    private Talon rightMotors;
+
+
+
+
+    public DriveSubsystem() {
+        leftMotors = new Talon(DriveConstants.LEFT_MOTORS);
+        rightMotors = new Talon(DriveConstants.RIGHT_MOTORS);
+
+
+        leftMotors.setInverted(DriveConstants.LEFT_INVERTED);
+        rightMotors.setInverted(DriveConstants.RIGHT_INVERTED);
+
+
+
+    }
+
+    public void drive(double leftSpeed, double rightSpeed) {
+        leftSpeed = MathUtil.applyDeadband(leftSpeed, .02);
+        rightSpeed = MathUtil.applyDeadband(rightSpeed, .02);
+
+        var speeds = DifferentialDrive.tankDriveIK(leftSpeed, rightSpeed, true);
+
+        leftMotors.set(speeds.left);
+        rightMotors.set(speeds.right);
+
+
+    }
+
+    public void stop() {
+        leftMotors.stopMotor();
+        rightMotors.stopMotor();
+    }
+
+}
