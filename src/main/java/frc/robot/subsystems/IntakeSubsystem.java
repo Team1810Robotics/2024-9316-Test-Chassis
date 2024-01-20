1f8e3cb099e013351a773b218a27a73bb19b49f8
Sarah Rutkowski <rutkosar000@usd232.org>
2024-01-20 09:30:31 -0600

    added sensors to intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
@@ -14,6 +15,10 @@ public class IntakeSubsystem extends SubsystemBase {
 
     private Talon intakePivitor;
 
+    private DigitalInput upperLS;
+    private DigitalInput lowerLS;
+    private DigitalInput noteDetector;
+
     public IntakeSubsystem() {
         leftIntake = new Talon(IntakeConstants.LEFT_INTAKE);
         rightIntake = new Talon(IntakeConstants.RIGHT_INTAKE);
@@ -23,6 +28,10 @@ public class IntakeSubsystem extends SubsystemBase {
 
         intakePivitor = new Talon(IntakeConstants.INTAKE_PIVITOR);
 
+        upperLS = new DigitalInput(IntakeConstants.UPPER_LS);
+        lowerLS = new DigitalInput(IntakeConstants.LOWER_LS);
+        noteDetector = new DigitalInput(IntakeConstants.NOTE_DETECTOR);
+
         leftIntake.addFollower(rightIntake);
         leftAdjuster.addFollower(rightAdjuster);
     };
@@ -31,6 +40,20 @@ public class IntakeSubsystem extends SubsystemBase {
         leftIntake.set(1);
 
         leftAdjuster.set(1);
+
+    }
+
+    public boolean getNoteDetector() {
+            return noteDetector.get();
+    }
+
+    public double getIntakeMotors() {
+        return leftIntake.get();
+    }
+
+    public void stopIntake() {
+        leftIntake.stopMotor();
+        leftAdjuster.stopMotor();
     }
 
     public void stop(){
