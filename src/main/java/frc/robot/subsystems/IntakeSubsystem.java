8d14df6c1015ea8955c6717bbd635a0eb3bee1c2
SBowling4 <bowlisam001@icloud.com>
2024-01-20 13:15:55 -0600

    Updated Intake to have Sparks, why would you lie to me carter?

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,17 +1,17 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-    private Talon leftIntake;
-    private Talon rightIntake;
+    private Relay intakeMotors;
 
-    private Talon leftAdjuster;
-    private Talon rightAdjuster;
+    private Relay adjusterMotors;
+
 
     private Talon intakePivitor;
 
@@ -21,11 +21,9 @@ public class IntakeSubsystem extends SubsystemBase {
 
 
     public IntakeSubsystem() {
-        leftIntake = new Talon(IntakeConstants.LEFT_INTAKE);
-        rightIntake = new Talon(IntakeConstants.RIGHT_INTAKE);
+        intakeMotors = new Relay(IntakeConstants.INTAKE_MOTORS);
 
-        leftAdjuster = new Talon(IntakeConstants.LEFT_ADJUSTER);
-        rightAdjuster = new Talon(IntakeConstants.RIGHT_ADJUSTER);
+        adjusterMotors = new Relay(IntakeConstants.ADJUSTER_MOTORS);
 
         intakePivitor = new Talon(IntakeConstants.INTAKE_PIVITOR);
 
@@ -33,14 +31,12 @@ public class IntakeSubsystem extends SubsystemBase {
         lowerLS = new DigitalInput(IntakeConstants.LOWER_LS);
         noteDetector = new DigitalInput(IntakeConstants.NOTE_DETECTOR);
 
-        leftIntake.addFollower(rightIntake);
-        leftAdjuster.addFollower(rightAdjuster);
 
     }
 
     public void intake() {
-        leftIntake.set(1);
-        leftAdjuster.set(1);
+        intakeMotors.set(Relay.Value.kForward);
+        adjusterMotors.set(Relay.Value.kForward);
 
     }
 
@@ -48,13 +44,13 @@ public class IntakeSubsystem extends SubsystemBase {
             return noteDetector.get();
     }
 
-    public double getIntakeMotors() {
-        return leftIntake.get();
+    public Relay.Value getIntakeMotors() {
+        return intakeMotors.get();
     }
 
     public void stopIntake() {
-        leftIntake.stopMotor();
-        leftAdjuster.stopMotor();
+        intakeMotors.stopMotor();
+        adjusterMotors.stopMotor();
     }
 
     public void pivotUp() {
@@ -75,8 +71,8 @@ public class IntakeSubsystem extends SubsystemBase {
 
 
     public void stop(){
-        leftIntake.stopMotor();
-        leftAdjuster.stopMotor();
+        intakeMotors.stopMotor();
+        intakeMotors.stopMotor();
 
         intakePivitor.stopMotor();
     }
