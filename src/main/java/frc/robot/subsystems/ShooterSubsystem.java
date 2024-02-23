77e29e6a627e7bd7842c757fa858db1931b9e21a
Sarah Rutkowski <rutkosar000@usd232.org>
2024-02-22 20:44:47 -0600

    Shooter shuffleboard stuff

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,7 +1,11 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
@@ -9,11 +13,37 @@ public class ShooterSubsystem extends SubsystemBase{
     private Talon leftShooterMotor;
     private Talon rightShooterMotor;
 
+    GenericEntry leftShooterSpeed;
+    GenericEntry rightShooterSpeed;
+
     public ShooterSubsystem(){
         leftShooterMotor = new Talon(ShooterConstants.LEFT_SHOOTER_MOTORS);
         rightShooterMotor = new Talon(ShooterConstants.RIGHT_SHOOTER_MOTOR);
+        ShuffleboardTab tab = Shuffleboard.getTab("Tab");
+        leftShooterSpeed = tab.add("leftShooterSpeed", leftShooterRunning()).getEntry();
+        rightShooterSpeed = tab.add("rightShooterSpeed", rightShooterRunning()).getEntry();
+    }
 
-    
+    public boolean leftShooterRunning() {
+        leftShooterMotor.get();
+        Double speed = leftShooterMotor.get();
+        if (speed > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean rightShooterRunning() {
+        rightShooterMotor.get();
+        Double speed = rightShooterMotor.get();
+        if (speed > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public void setSpeakerSpeed() {
